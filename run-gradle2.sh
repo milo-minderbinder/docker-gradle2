82be1ce53761d1001323e678d6ddfd94af1f58eb
Milo Minderbinder <minderbinder.enterprises@gmail.com>
2015-03-10 21:46:39 -0400

    run-gradle2.sh now autodetects maven and gradle installations on Docker host via M2_HOME and GRADLE_USER_HOME env vars, and will mount the directories pointed to by those env vars in the container. This will allow the container to utilize the local Gradle and Maven repos, instead of building new repos from scratch with each new container.

--- run-gradle2.sh
+++ run-gradle2.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+mnt_gradle=""
+if [[ -n "$GRADLE_USER_HOME" ]]; then
+	mnt_gradle="-v $GRADLE_USER_HOME:/mnt/.gradle"
+	echo "Will mount \$GRADLE_USER_HOME with: '${mnt_gradle}'"
+fi
+
+mnt_m2=""
+if [[ -n "$M2_HOME" ]]; then
+	mnt_m2="-v $M2_HOME:/mnt/.m2"
+	echo "Will mount \$M2_HOME with: '${mnt_m2}'"
+fi
 
 usage() {
 	echo "Usage: ${0} [-t <tag>] [<path/to/volume>]"
@@ -25,4 +36,4 @@ if [[ -n "$1" ]]; then
 fi
 
 echo "Running gradle2:${tag} image with ${gradle_projects_dir} mounted on /mnt/gradle-projects"
-docker run -it --rm -v ${gradle_projects_dir}:/mnt/gradle-projects --name gradle2 mminderbinder/gradle2:${tag} bash
+docker run -it --rm $mnt_gradle $mnt_m2 -v ${gradle_projects_dir}:/mnt/gradle-projects --name gradle2 mminderbinder/gradle2:${tag} bash
